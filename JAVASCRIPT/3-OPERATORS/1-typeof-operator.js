db74e6005a6a5ddb83c5e1cf42ea2ef31b859709
Coder-SagarThakare <saggythakare01@gmail.com>
2024-10-13 01:00:22 +0530

    object added

---
+++ JAVASCRIPT/3-OPERATORS/1-typeof-operator.js
@@ -0,0 +1,10 @@
+// typeof operator
+
+console.log(typeof 12);     // number
+
+console.log(typeof 12.7);   // number
+
+console.log(typeof 15n);    // bigint
+
+console.log(typeof null);   // empty object
+
---
+++ JAVASCRIPT/3-OPERATORS/4-explicitType.js
@@ -0,0 +1,20 @@
+
+
+// type conversion
+
+// explicit conversion => je user/developer swatha krto jenekrun aaplyala pahijel tya data type mdhe value bhetel
+
+const num1 = Number("45");
+const num2 = Number("42.7");
+
+var value = "any value"
+const str = String(value);
+
+const bool1 = Boolean("value");     // true false
+const bool2 = Boolean("");          // true false
+
+
+//converte sagar to number
+
+
+
