da94bbb07e9024f635b5a932ef9cabc473afcd4f
Coder-SagarThakare <saggythakare01@gmail.com>
2024-12-11 20:58:36 +0530

    for loop added

---
+++ JAVASCRIPT/4-CONTROL-FLOW-STATEMENT/3-for-loop.js
@@ -0,0 +1,74 @@
+// FOR LOOP
+
+// for loop used for doing repeatated work/task
+//      1               2           4
+// for(initialization; condition; inc/dec){
+// repetated task           //3
+// }
+
+// ---------------------------------------------------------
+
+// increment for loop
+// for (let i = 0; i < 5; i++)
+//     console.log(i);                 // 0 1 2 3 4
+// ---------------------------------------------------------
+
+// decrement for loop
+// let count ;
+
+// for( count=7 ; count >0 ; count-- )
+//     console.log(count);
+
+// ---------------------------------------------------------
+
+// find even no between 1-10
+var value = 6;
+
+// even => 2 4 6 8 10
+
+// for(let i=1; i<=10; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+// ---------------------------------------------------------
+
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+// var userValue = 4;
+// var gap = 8;
+
+// let i;      // 4+8 = 12+8 = 20+8 = 28+8 = 36+8 = 44
+
+// for (i = userValue; i <= 43; i = i + gap) {
+//     console.log(i)          // ... 36
+// }
+
+// console.log("i", i);
+
+// ---------------------------------------------------------
+
+//  access values of Array using for loop
+
+// var marks = [2, 4, 43, 67, 43, 23, 45];
+
+// let i;
+// for (i = 0; i < marks.length; i++) {                
+//     console.log(marks[i])          
+// }
+
+// console.log("i", i);
+// ---------------------------------------------------------
+
+// find count of array of numbers array
+// var num = [43, 67, 45, 98, 12, 56, 87, 9, 34, 65, 23];
+
+// var count = 0;
+
+// for (let i = 0; typeof (num[i]) != "undefined"; i++) {
+//     count = count + 1;
+// }
+
+// console.log("count", count);
+
+// ------------------------------------------------------------
+
---
+++ JAVASCRIPT/p1.js
@@ -0,0 +1,19 @@
+
+// for loop syntax
+// Loops from 1 to 5.
+// common mistake => use semicolon after loop
+// Decrementing Loop
+// Loops from 0 to 10, incrementing by 2.
+// Access each element in an array.
+// Breaking a Loop
+// Skipping an Iteration (continue)(Skip even numbers from 1 to 10.)
+// Loop Without Initialization => Counter variable defined outside the loop
+// Infinite Loop
+// Reversing an Array => print elements in reverse order
+// Using for to Calculate a Sum
+// Loop with Multiple Variables
+// for (let i = 1, j = 5; i <= 5 && j >= 1; i++, j--) {
+
+//  Nested for Loop.
+
+
