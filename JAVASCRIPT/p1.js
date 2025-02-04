5ecf4b7e56091013ab9970194ff3c09e51411600
Coder-SagarThakare <saggythakare01@gmail.com>
2025-02-04 23:53:52 +0530

    for loop code added

--- JAVASCRIPT/p1.js
+++ JAVASCRIPT/p1.js
@@ -44,13 +44,83 @@
 
 
 
-let rows = 4, cols = 6;
+// let rows = 4, cols = 6;
 
-for (let i = 0; i < rows; i++) {       // i represnt to rows i= 0 1 2 3 4
+// for (let i = 0; i < rows; i++) {       // i represnt to rows i= 0 1 2 3 4
 
-    for (let j = 0; j < cols; j++) {   
-        process.stdout.write(`* `);
+//     for (let j = 0; j < cols; j++) {   
+//         process.stdout.write(`* `);
+//     }
+
+//     console.log();
+// }
+
+
+// --------------------------------------------------------
+
+// star pattern using string
+
+
+
+// printStarPattern(3,7);
+
+
+
+//parameter = rowsLength,colsLength
+// var count = 0; //0 1 2 3 4 5 0 1 2 3 4 5 0
+
+function printStarPattern(rowsLength = 3, colsLength = 3) {// rowlength=5 colsLength=5
+
+    // outer for loop => row
+    for (let row = 0; row < rowsLength; row++) { // row =0 1
+
+        // inner for loop => column
+        for (let col = 0; col < colsLength; col++) { //
+            process.stdout.write(`${row} `)
+        }
+
+        console.log()
     }
-
-    console.log();
 }
+// arguments (5,5)
+// printStarPattern(2,5);
+
+// console.log("----------------------");
+
+// ------------------------------------------------------------------------
+// (5,5)
+
+
+
+// *         
+// * *       
+// * * *     
+// * * * *   
+// * * * * * 
+
+// * * * * * 
+// * * * *   
+// * * *     
+// * *       
+// *
+
+function print(length) {     // length =5
+
+    for (let i = 0; i < length; i++) {     // row    i=0 1 2
+
+        for (let j = 0; j < length; j++) {        // cols   j=0 12
+            if (j <=  i) {
+                process.stdout.write("* ");
+            } else {
+                process.stdout.write("  ");
+            }
+        }
+        console.log();
+    }
+}
+
+print(5)
+console.log("-----------------------------------")
+// print(10)
+console.log("-----------------------------------")
+// print(4)
