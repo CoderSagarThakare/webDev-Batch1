6ed2555f7134a3e0a9414a8191db6d71c41234f5
Coder-SagarThakare <saggythakare01@gmail.com>
2025-02-13 02:17:17 +0530

    for loop pattern added

--- JAVASCRIPT/p1.js
+++ JAVASCRIPT/p1.js
@@ -1,73 +1,4 @@
 
-// for loop syntax
-// Loops from 1 to 5.
-// common mistake => use semicolon after loop
-// Decrementing Loop
-// Loops from 0 to any limit , incrementing by specific gap.
-
-// print table of 5
-// print factorial of number
-// Access each element in an array.
-
-// Print the reverse of a number (e.g., 123 becomes 321).
-// Break and continue statement in for loop
-
-// Write a for loop to convert all characters in a string to uppercase.
-// Loop Without Initialization => Counter variable defined outside the loop
-// Infinite Loop
-// Reversing an Array => print elements in reverse order
-// Using for to Calculate a Sum
-// Loop with Multiple Variables
-// for (let i = 1, j = 5; i <= 5 && j >= 1; i++, j--) {
-
-//  Nested for Loop.
-
-
-
-// NESTED FOR LOOP
-
-// * * * *
-// * * * *
-// * * * *
-// * * * *
-
-// let rows = 4, cols = 4;
-
-// for (let i = 0; i < rows; i++) {       // i represnt to rows i= 0 1 2 3 4
-
-//     for (let j = 0; j < cols; j++) {   
-//         process.stdout.write(`* `);
-//     }
-
-//     console.log();
-// }
-
-
-
-// let rows = 4, cols = 6;
-
-// for (let i = 0; i < rows; i++) {       // i represnt to rows i= 0 1 2 3 4
-
-//     for (let j = 0; j < cols; j++) {   
-//         process.stdout.write(`* `);
-//     }
-
-//     console.log();
-// }
-
-
-// --------------------------------------------------------
-
-// star pattern using string
-
-
-
-// printStarPattern(3,7);
-
-
-
-//parameter = rowsLength,colsLength
-// var count = 0; //0 1 2 3 4 5 0 1 2 3 4 5 0
 
 function printStarPattern(rowsLength = 3, colsLength = 3) {// rowlength=5 colsLength=5
 
@@ -82,35 +13,21 @@ function printStarPattern(rowsLength = 3, colsLength = 3) {// rowlength=5 colsLe
         console.log()
     }
 }
-// arguments (5,5)
-// printStarPattern(2,5);
-
-// console.log("----------------------");
-
-// ------------------------------------------------------------------------
-// (5,5)
 
 
+// 
+const a = "sagar";      //9 = 5  (9/2= 4.5)   Math.ceil(4.5)=>5
+console.log(a[4])  // subscripts
 
-// *         
-// * *       
-// * * *     
-// * * * *   
-// * * * * * 
+function print(row) {
 
-// * * * * * 
-// * * * *   
-// * * *     
-// * *       
-// *
+    let num = 1;
+    for (let i = 0; i < row; i++) {  // 2
+        
+         for (let j = 0; j < row * 2 - 1; j++) { //234567
 
-function print(length) {     // length =5
-
-    for (let i = 0; i < length; i++) {     // row    i=0 1 2
-
-        for (let j = 0; j < length; j++) {        // cols   j=0 12
-            if (j <=  i) {
-                process.stdout.write("* ");
+            if (j >= (row - i - 1) && j <= (row - 1 + i)) {
+                process.stdout.write(`${a[i]} `);
             } else {
                 process.stdout.write("  ");
             }
@@ -118,9 +35,9 @@ function print(length) {     // length =5
         console.log();
     }
 }
-
-print(5)
-console.log("-----------------------------------")
-// print(10)
-console.log("-----------------------------------")
+const b = "college"         //7 = 4  (7/2= 3.5)
+console.log("----- input - 5 -------------------");
+print(alength())
+console.log("----- input - 4 --------------------");
 // print(4)
+// print(7)
