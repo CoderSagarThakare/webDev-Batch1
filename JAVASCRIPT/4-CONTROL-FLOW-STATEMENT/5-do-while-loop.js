026d9022369bee47414e86a3b4b0797775c9f412
Coder-SagarThakare <saggythakare01@gmail.com>
2025-03-03 23:40:39 +0530

    do while added

--- JAVASCRIPT/4-CONTROL-FLOW-STATEMENT/5-do-while-loop.js
+++ JAVASCRIPT/4-CONTROL-FLOW-STATEMENT/5-do-while-loop.js
@@ -2,3 +2,27 @@
 // do {
 //
 // } while(condition)
+
+//-----------------------------------------------------------------------------
+
+// Bank data
+let savedPin = 2925;
+let count = 0;
+// user data
+let usersEnteredPin;
+
+do {
+  usersEnteredPin = Number(prompt("please Enter your correct ATM pin"));
+  count++;
+
+  console.log(usersEnteredPin);
+  console.log(count);
+} while (usersEnteredPin != savedPin && count < 3);
+
+if (usersEnteredPin == savedPin) {
+  alert("Now You can withdraw Cash...");
+} else {
+  alert("your card blocked for 24 hr");
+}
+
+//-----------------------------------------------------------------------------
