cfaf9f3cf907644b45b05ab6814ff70b5ae39303
Coder-SagarThakare <saggythakare01@gmail.com>
2025-04-01 22:33:45 +0530

    event T05 added

---
+++ JAVASCRIPT/SOLUTIONS/mouse-keyboard-events/T05-change-image-src/index.js
@@ -0,0 +1,17 @@
+const img = document.getElementById("img");
+const btn = document.getElementsByClassName("btn");
+
+btn[0].addEventListener("click", (e) => {
+  img.src =
+    "https://tse3.mm.bing.net/th?id=OIP.1bnxcVBT_bKoNqP-tLjcpgHaJf&pid=Api&P=0&h=180";
+});
+
+btn[1].addEventListener("click", (e) => {
+  img.src =
+    "https://tse4.mm.bing.net/th?id=OIP.yUIb5S_kj98Eg5tT-Onx1AHaHa&pid=Api&P=0&h=180";
+});
+
+btn[2].addEventListener("click", (e) => {
+  img.src =
+    "https://tse3.mm.bing.net/th?id=OIP.0qThwGUlnULLAz8MTnN9QwHaHa&pid=Api&P=0&h=180";
+});
